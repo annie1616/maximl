0df3ef2eeb71d53d3be354a00175b028abfb73c1
annie1616 <52657958+annie1616@users.noreply.github.com>
2019-07-08 16:09:39 +0530

    Update mi.py

--- mi.py
+++ mi.py
@@ -1,1 +1,37 @@
 
+ 
+def sequence(st): 
+    n = len(st) 
+    m= 1
+    distlen = 1  
+    p = -1    
+    i = 0   
+    hasharr = [0] * 256 
+    
+   
+    for i in range(n):
+        if st[i]>='a' and st[i]<='z':
+            p = hasharr[ord(st[i])] 
+            if p == -1 or (i - m > p): 
+                m+= 1
+        
+            else: 
+           
+                if m > distlen: 
+                    distlen = m 
+  
+                m = i - p 
+         
+            hasharr[ord(st[i])] = i
+        else:
+            return 0
+  
+    
+        if m > distlen: 
+            distlen = m 
+  
+    return distlen 
+  
+
+st=input()
+print(sequence(st))
